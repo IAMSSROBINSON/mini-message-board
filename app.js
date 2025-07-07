700857f840884461d21cce18e56b83ff5711f631
SSR <iamssrobinson@gmail.com>
2025-07-07 08:45:37 +0100

    Startup message.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+console.log("App is starting...");
 
 app.get('/', (req, res) => {
     res.send("Hello World From Render!");
