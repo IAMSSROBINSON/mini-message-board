6c06dec818732cfc6b5d6cd15e0058707c58871c
SSR <iamssrobinson@gmail.com>
2025-07-07 08:23:07 +0100

    Initial app setup.

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World From Render!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}..`);
+});
+
+
